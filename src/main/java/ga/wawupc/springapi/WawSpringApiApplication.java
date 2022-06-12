1e74b5f7232f434a9668042c6b369aae3f370d20
Diego Albitres <diego.albitres@gmail.com>
2022-06-12 03:31:41 -0500

    Initial commit

---
+++ src/main/java/ga/wawupc/springapi/WawSpringApiApplication.java
@@ -0,0 +1,13 @@
+package ga.wawupc.springapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WawSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WawSpringApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ga/wawupc/springapi/WawSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.wawupc.springapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WawSpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
