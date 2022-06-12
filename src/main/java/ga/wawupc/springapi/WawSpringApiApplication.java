71554caabcac31d6f9e1bc98a90d6183e238a86a
Diego Albitres <diego.albitres@gmail.com>
2022-06-12 18:54:52 -0500

    style: added editorconfig rules and reapplied formatting

--- src/main/java/ga/wawupc/springapi/WawSpringApiApplication.java
+++ src/main/java/ga/wawupc/springapi/WawSpringApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WawSpringApiApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(WawSpringApiApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(WawSpringApiApplication.class, args);
+  }
 }
