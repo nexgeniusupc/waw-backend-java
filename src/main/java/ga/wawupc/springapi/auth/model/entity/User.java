461867cd8324b49d3e2fec984806e5cd1ec30416
Kolya Castañeda <gakol8@hotmail.com>
2022-06-13 18:59:21 -0500

    feat(users): created User entity

---
+++ src/main/java/ga/wawupc/springapi/auth/model/entity/User.java
@@ -0,0 +1,43 @@
+package ga.wawupc.springapi.auth.model.entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@With
+@Entity
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotNull
+  @NotBlank
+  private String FullName;
+
+  @NotNull
+  @NotBlank
+  private String PreferredName;
+
+  @NotNull
+  @NotBlank
+  private String email;
+
+  @NotNull
+  @NotBlank
+  private Date birthdate;
+
+  private String location;
+
+  private String biography;
+
+  private String about;
+}
