0f75feb7e1be54fc962750e2e5528f1ab40b3831
Angel Meneses <angelmenesest123@gmail.com>
2022-06-13 18:48:19 -0500

    refactor: added validation annotations

--- src/main/java/ga/wawupc/springapi/employers/domain/model/entity/Company.java
+++ src/main/java/ga/wawupc/springapi/employers/domain/model/entity/Company.java
@@ -1,10 +1,12 @@
 package ga.wawupc.springapi.employers.domain.model.entity;
 
+import ga.wawupc.springapi.shared.domain.model.AuditModel;
 import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -12,18 +14,23 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 @With
 @Entity
-public class Company {
+// @Table(name = "companies")
+public class Company extends AuditModel {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @NotNull
   @NotBlank
+  @Size(max = 100)
+  @Column(unique = true)
   private String name;
 
+  @Size(max = 256)
   private String address;
 
   @NotNull
   @NotBlank
+  @Size(max = 256)
   private String email;
 }
