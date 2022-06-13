7823eb0aca7b8380103a153c2cc80153689151db
Angel Meneses <angelmenesest123@gmail.com>
2022-06-12 21:10:40 -0500

    feat(companies): created Company resource for POST request

---
+++ src/main/java/ga/wawupc/springapi/employers/resource/CreateCompanyResource.java
@@ -0,0 +1,21 @@
+package ga.wawupc.springapi.employers.resource;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class CreateCompanyResource {
+  @NotNull
+  @NotBlank
+  @Size(max = 100)
+  private String name;
+
+  @Size(max = 256)
+  private String address;
+
+  @NotNull
+  @NotBlank
+  @Size(max = 256)
+  private String email;
+
+}
