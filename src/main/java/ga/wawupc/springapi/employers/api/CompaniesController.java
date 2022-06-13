0a2ddcd00d98203e78a42c8049b286e4b510d75d
Angel Meneses <angelmenesest123@gmail.com>
2022-06-13 18:49:29 -0500

    refactor: updated inbound parameters and added annotations

--- src/main/java/ga/wawupc/springapi/employers/api/CompaniesController.java
+++ src/main/java/ga/wawupc/springapi/employers/api/CompaniesController.java
@@ -1,16 +1,17 @@
 package ga.wawupc.springapi.employers.api;
 
-import ga.wawupc.springapi.employers.domain.persistence.CompanyRepository;
 import ga.wawupc.springapi.employers.domain.service.CompanyService;
 import ga.wawupc.springapi.employers.mapping.CompanyMapper;
+import ga.wawupc.springapi.employers.resource.CompanyRequest;
 import ga.wawupc.springapi.employers.resource.CompanyResource;
-import ga.wawupc.springapi.employers.resource.CreateCompanyResource;
-import ga.wawupc.springapi.employers.resource.UpdateCompanyResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+// @Tag(name = "Companies")
+@RestController
+@RequestMapping("/api/v1/companies")
 public class CompaniesController {
   private final CompanyService service;
   private final CompanyMapper mapper;
@@ -20,6 +21,7 @@ public class CompaniesController {
     this.mapper = mapper;
   }
 
+  // @Operation(summary = "Get Companies", description = "Get all companies" )
   @GetMapping
   public Page<CompanyResource> getAllCompanies(Pageable pageable){
     return mapper.modelListPage(service.getAll(), pageable);
@@ -31,12 +33,12 @@ public class CompaniesController {
   }
 
   @PostMapping
-  public CompanyResource createCompany(@RequestBody CreateCompanyResource company){
+  public CompanyResource createCompany(@RequestBody CompanyRequest company){
     return mapper.toResource(service.create(mapper.toModel(company)));
   }
 
   @PutMapping("{id}")
-  public CompanyResource updateCompany(@PathVariable Long id, @RequestBody UpdateCompanyResource company){
+  public CompanyResource updateCompany(@PathVariable Long id, @RequestBody CompanyRequest company){
     return mapper.toResource(service.update(id, mapper.toModel(company)));
   }
 
