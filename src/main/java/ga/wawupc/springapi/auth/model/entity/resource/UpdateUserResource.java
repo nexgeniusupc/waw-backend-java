a6a15adc7f9a1c2c1265a2d39f578e270cf6c534
Kolya Castañeda <gakol8@hotmail.com>
2022-06-13 19:24:01 -0500

    feat(users): created User resource for PUT

---
+++ src/main/java/ga/wawupc/springapi/auth/model/entity/resource/UpdateUserResource.java
@@ -0,0 +1,39 @@
+package ga.wawupc.springapi.auth.model.entity.resource;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+public class UpdateUserResource {
+private Long id;
+
+  @NotNull
+  @NotBlank
+  @Size(max = 256)
+  private String fullName;
+
+  @NotNull
+  @NotBlank
+  @Size(max = 256)
+  private String preferredName;
+
+  @NotNull
+  @NotBlank
+  @Size(max = 256)
+  private String email;
+
+  @NotNull
+  @NotBlank
+  @Size(max = 10)
+  private Date birthdate;
+
+  @Size(max = 256)
+  private String location;
+
+  @Size(max = 256)
+  private String biography;
+
+  @Size(max = 256)
+  private String about;
+}
