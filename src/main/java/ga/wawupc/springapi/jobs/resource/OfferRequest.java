e61ddefb3498c9b75b165db6f998fb492a867be8
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-06-13 22:09:33 -0500

    refactor: unified create and update offer resources into request model

--- src/main/java/ga/wawupc/springapi/jobs/resource/OfferRequest.java
+++ src/main/java/ga/wawupc/springapi/jobs/resource/OfferRequest.java
@@ -5,7 +5,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-public class CreateOfferResource {
+public class OfferRequest {
 
   @NotNull
   @NotBlank
