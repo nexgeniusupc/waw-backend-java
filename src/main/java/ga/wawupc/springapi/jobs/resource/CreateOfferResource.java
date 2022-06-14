4f0879a1f677ecbd017b8142bf28c011fc3d6029
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-06-13 20:06:31 -0500

    feat(offers): implemented Offer resource for POST request
    Offer resource for POST request were created and restricted.

---
+++ src/main/java/ga/wawupc/springapi/jobs/resource/CreateOfferResource.java
@@ -0,0 +1,27 @@
+package ga.wawupc.springapi.jobs.resource;
+
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class CreateOfferResource {
+
+  @NotNull
+  @NotBlank
+  @Size(max=200)
+  private String title;
+  @NotNull
+  @NotBlank
+  @Size(max=700)
+  private String image;
+  @NotNull
+  @NotBlank
+  private String description;
+
+  @Size(max=400)
+  private String salaryRange;
+  @NotNull
+  @NotBlank
+  private Boolean status;
+}
