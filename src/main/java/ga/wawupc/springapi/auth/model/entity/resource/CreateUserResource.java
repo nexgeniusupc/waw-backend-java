c3dc4b6522a663f116bdcd47d66f9170f1699289
Kolya Castañeda <gakol8@hotmail.com>
2022-06-13 19:14:39 -0500

    feat(users): created User resource for POST

---
+++ src/main/java/ga/wawupc/springapi/auth/model/entity/resource/CreateUserResource.java
@@ -0,0 +1,37 @@
+package ga.wawupc.springapi.auth.model.entity.resource;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+public class CreateUserResource {
+  @NotNull
+  @NotBlank
+  @Size(max = 256)
+  private String FullName;
+
+  @NotNull
+  @NotBlank
+  @Size(max = 256)
+  private String PreferredName;
+
+  @NotNull
+  @NotBlank
+  @Size(max = 256)
+  private String email;
+
+  @NotNull
+  @NotBlank
+  @Size(max = 10)
+  private Date birthdate;
+
+  @Size(max = 256)
+  private String location;
+
+  @Size(max = 256)
+  private String biography;
+
+  @Size(max = 256)
+  private String about;
+}
