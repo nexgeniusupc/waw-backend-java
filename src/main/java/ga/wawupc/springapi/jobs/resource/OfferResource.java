3427986dec0d21dfc6913de3b8b819f733a8ae8d
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-06-13 20:04:13 -0500

    feat(offer): update base Offer resource
    Resource of the offer service were created.

--- src/main/java/ga/wawupc/springapi/jobs/resource/OfferResource.java
+++ src/main/java/ga/wawupc/springapi/jobs/resource/OfferResource.java
@@ -1,20 +1,19 @@
 package ga.wawupc.springapi.jobs.resource;
 
 import lombok.*;
-
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class OfferResource {
-  @Getter
-  @Setter
-  @With
-  @NoArgsConstructor
-  @AllArgsConstructor
-  public class CompanyResource {
-    private Long id;
-    private String title;
-    private String image;
-    private String description;
-    private String salaryRange;
-    private Boolean status;
-  }
+
+  private Long id;
+  private String title;
+  private String image;
+  private String description;
+  private String salaryRange;
+  private Boolean status;
+
 }
