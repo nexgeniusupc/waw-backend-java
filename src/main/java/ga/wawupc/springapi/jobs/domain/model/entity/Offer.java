33ea13b533811b2f4e47f77eeaf8605a61af7862
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-06-13 19:40:04 -0500

    feat(offer): created and implemented Offer entity model
    The properties of the offer entity were created and restricted.

---
+++ src/main/java/ga/wawupc/springapi/jobs/domain/model/entity/Offer.java
@@ -0,0 +1,40 @@
+package ga.wawupc.springapi.jobs.domain.model.entity;
+
+import ga.wawupc.springapi.shared.domain.model.AuditModel;
+import lombok.*;
+import javax.persistence.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "offers")
+public class Offer extends AuditModel {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotNull
+  @NotBlank
+  @Size(max=200)
+  private String title;
+  @NotNull
+  @NotBlank
+  @Size(max=700)
+  private String image;
+  @NotNull
+  @NotBlank
+  private String description;
+
+  @Size(max=400)
+  private String salaryRange;
+  @NotNull
+  @NotBlank
+  private Boolean status;
+}
