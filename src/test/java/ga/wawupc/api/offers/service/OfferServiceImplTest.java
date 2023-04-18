4ebc241c0b94a789f7f8248a0e5eaee0b3d57f24
Kolya Castañeda <gakol8@hotmail.com>
2023-04-18 17:00:59 -0500

    test(offers): added test to assert exception throwing when offer with existing name is created

--- src/test/java/ga/wawupc/api/offers/service/OfferServiceImplTest.java
+++ src/test/java/ga/wawupc/api/offers/service/OfferServiceImplTest.java
@@ -1,17 +1,42 @@
 package ga.wawupc.api.offers.service;
 
 
+import ga.wawupc.api.jobs.domain.model.entity.Offer;
 import ga.wawupc.api.jobs.domain.persistence.OfferRepository;
 import ga.wawupc.api.jobs.service.OfferServiceImpl;
+
+import ga.wawupc.api.shared.exception.ResourceValidationException;
+
+import jakarta.validation.Validator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class OfferServiceUnitTest {
   @Mock
   private OfferRepository repository;
+  @Mock
+  private Validator validator;
   @InjectMocks
   private OfferServiceImpl service;
+  @Test
+  public void createCompanyWithExistingNameShouldThrowException() {
+    Offer offer1 = new Offer(1L, "Full Stack Developer" , "https://example.com/image.jpg","We're looking for a full stack developer with experience in Java and Angular.", "$125k-$127k", true);
+    Offer offer2 = new Offer(2L, "Full Stack Developer" , "https://example.com/image.jpg","We're looking for a backend developer with experience in JavaScript and Vue.", "$200k-$250k", true);
+
+    when(repository.findByTitle(offer1.getTitle())).thenReturn(offer1);
+
+    when(validator.validate(offer2)).thenReturn(Collections.emptySet());
+
+    Assertions.assertThrows(ResourceValidationException.class, () -> service.create(offer2));
+  }
 }
