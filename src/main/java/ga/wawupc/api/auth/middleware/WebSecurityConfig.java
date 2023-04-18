af09ca00bf440f4afbd432fc6df94fb89de872d0
Diego Albitres <diego.albitres@gmail.com>
2023-04-17 19:25:18 -0500

    refactor(auth): rewrite `WebSecurityConfig` to not use deprecated packages

--- src/main/java/ga/wawupc/api/auth/middleware/WebSecurityConfig.java
+++ src/main/java/ga/wawupc/api/auth/middleware/WebSecurityConfig.java
@@ -3,21 +3,20 @@ package ga.wawupc.api.auth.middleware;
 import ga.wawupc.api.auth.domain.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
   private final UserService userService;
 
   JwtAuthenticationEntryPoint unauthorizedHandler;
@@ -41,22 +40,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   }
 
   @Bean
-  @Override
-  public AuthenticationManager authenticationManager() throws Exception {
-    return super.authenticationManagerBean();
-  }
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.cors().and().csrf().disable()
-      .exceptionHandling()
-      .authenticationEntryPoint(unauthorizedHandler).and()
-      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-      .authorizeHttpRequests().antMatchers(
-        "/api/v1/companies/*",
-        "/api/v1/offers/*"
-      ).authenticated()
-      .anyRequest().permitAll();
-    http.addFilterBefore(authorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http
+        .cors().and().csrf().disable()
+        .addFilterBefore(authorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+        .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+        .authorizeHttpRequests()
+        .requestMatchers("/_/actuator/health").permitAll()
+        .requestMatchers("/api/v1/auth/**").permitAll()
+        .anyRequest().authenticated()
+        .and().build();
   }
 }
