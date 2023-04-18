a944d4743e7700f5c5d900f974dadc70a04ec135
Angel Meneses <angelmenesest123@gmail.com>
2023-04-17 20:47:10 -0500

    test(companies): set up the unit test class mocking the repository

---
+++ src/test/java/ga/wawupc/api/employers/service/CompanyServiceImplTest.java
@@ -0,0 +1,25 @@
+package ga.wawupc.api.employers.service;
+
+import ga.wawupc.api.employers.domain.model.entity.Company;
+import ga.wawupc.api.employers.domain.persistence.CompanyRepository;
+import ga.wawupc.api.shared.exception.ResourceValidationException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CompanyServiceUnitTest {
+  @Mock
+  private CompanyRepository repository;
+  @InjectMocks
+  private CompanyServiceImpl service;
+
+}
