146aa81458f8735e7f55188d7cc33a1570cb3db1
jrenzzo <u202118568@upc.edu.pe>
2023-04-18 16:47:33 -0500

    test(offers): Implement test case for non-existing offer in Find by ID method.

--- src/test/java/ga/wawupc/api/offers/services/OfferServiceImplTest.java
+++ src/test/java/ga/wawupc/api/offers/services/OfferServiceImplTest.java
@@ -2,15 +2,16 @@ package ga.wawupc.api.offers.services;
 import ga.wawupc.api.jobs.domain.model.entity.Offer;
 import ga.wawupc.api.jobs.domain.persistence.OfferRepository;
 import ga.wawupc.api.jobs.service.OfferServiceImpl;
+import ga.wawupc.api.shared.exception.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
-import org.mockito.internal.matchers.Null;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -33,5 +34,14 @@ public class OfferServiceImplTest {
     Offer response = offerServiceImpl.getById(1L);
     assertEquals(1L, response.getId());
     verify(offerRepository).findById(offer.getId());
+  }
+
+  @Test
+  public void FindByIdNonExistingOfferShouldThrowException(){
+    when(offerRepository.findById(offer.getId())).thenReturn(Optional.empty());
+
+    Throwable exception =
+      catchThrowable(() -> offerServiceImpl.getById(offer.getId()));
+    assertThat(exception).isInstanceOf(ResourceNotFoundException.class);
   }
 }
