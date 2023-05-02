2ab1ebe1fdc931c969f3adee1b326ac74b1309ba
jrenzzo <u202118568@upc.edu.pe>
2023-05-02 18:42:07 -0500

    test(offers): implement integration tests for jobs endpoints

--- src/test/java/ga/wawupc/api/offers/OffersIntegrationTest.java
+++ src/test/java/ga/wawupc/api/offers/OffersIntegrationTest.java
@@ -16,8 +16,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import java.util.Arrays;
 import java.util.List;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,7 +31,7 @@ public class OffersIntegrationTest {
   private MockMvc mvc;
   @Autowired
   private OfferRepository repository;
-  private static final Offer firstTestOffer = new Offer(1L, "Vue Mid Jr. Developer", "https://example.com/images/full-stack-developer.png", "We're seeking a skilled full stack developer to join our team and help us build and maintain cutting-edge web applications. The ideal candidate will have experience with a range of technologies and be comfortable working in a fast-paced, agile environment.", "$80,000 - $120,000", true);
+  private static final Offer firstTestOffer = new Offer(1L, "Vue Mid Sr. Developer", "https://example.com/images/full-stack-developer.png", "We're seeking a skilled full stack developer to join our team and help us build and maintain cutting-edge web applications. The ideal candidate will have experience with a range of technologies and be comfortable working in a fast-paced, agile environment.", "$80,000 - $120,000", true);
   private static final Offer secondTestOffer = new Offer(2L, "Software Engineer (React)", "https://example.com/images/frontend-engineer.png", "We're looking for a talented software engineer with a strong focus on frontend development to join our team. The ideal candidate will have experience building complex web applications with modern JavaScript frameworks, and a passion for creating beautiful and intuitive user interfaces.", "$90,000 - $140,000", true);
   private static final List<Offer> offers = Arrays.asList(firstTestOffer, secondTestOffer);
 
@@ -44,6 +45,16 @@ public class OffersIntegrationTest {
     repository.deleteAll();
     repository.flush();
   }
+  @Test
+  public void getAllOffersIntegrationTest() throws Exception {
+    RequestBuilder request = get("/api/v1/offers").contentType(MediaType.APPLICATION_JSON);
+
+    mvc.perform(request)
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.content", hasSize(2)))
+      .andExpect(jsonPath("$.content[0].title", is(firstTestOffer.getTitle())))
+      .andExpect(jsonPath("$.content[1].title", is(secondTestOffer.getTitle())));
+  }
 
   @Test
   public void addOfferIntegrationTest() throws Exception {
@@ -56,5 +67,16 @@ public class OffersIntegrationTest {
     mvc.perform(request)
       .andExpect(status().isOk())
       .andExpect(jsonPath("$.title", is(dataScientistOffer.getTitle())));
+  }
+
+  @Test
+  public void addOfferAlreadyExistingIntegrationTest() throws Exception {
+    OfferRequest offer2 = new OfferRequest("Vue Mid Sr. Developer", "https://example.com/images/full-stack-developer.png", "We're seeking a skilled full stack developer to join our team and help us build and maintain cutting-edge web applications. The ideal candidate will have experience with a range of technologies and be comfortable working in a fast-paced, agile environment.", "$100,000 - $150,000 per year", true );
+
+    RequestBuilder request = post("/api/v1/offers")
+      .contentType(MediaType.APPLICATION_JSON)
+      .content(JsonUtil.toJson(offer2));
+
+    mvc.perform(request).andExpect(status().isBadRequest());
   }
 }
