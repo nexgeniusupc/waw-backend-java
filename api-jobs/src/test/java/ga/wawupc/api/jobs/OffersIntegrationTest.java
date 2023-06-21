b375a041f90319fd23f56ca0ce09ae949fd2ea57
Diego Albitres <diego.albitres@gmail.com>
2023-06-20 22:45:38 -0500

    test(offers): join separated tests from previous project

--- api-jobs/src/test/java/ga/wawupc/api/jobs/OffersIntegrationTest.java
+++ api-jobs/src/test/java/ga/wawupc/api/jobs/OffersIntegrationTest.java
@@ -32,8 +32,14 @@ public class OffersIntegrationTest {
   private MockMvc mvc;
   @Autowired
   private OfferRepository repository;
-  private static final Offer firstTestOffer = new Offer(1L, "Vue Mid Sr. Developer", "https://example.com/images/full-stack-developer.png", "We're seeking a skilled full stack developer to join our team and help us build and maintain cutting-edge web applications. The ideal candidate will have experience with a range of technologies and be comfortable working in a fast-paced, agile environment.", "$80,000 - $120,000", true);
-  private static final Offer secondTestOffer = new Offer(2L, "Software Engineer (React)", "https://example.com/images/frontend-engineer.png", "We're looking for a talented software engineer with a strong focus on frontend development to join our team. The ideal candidate will have experience building complex web applications with modern JavaScript frameworks, and a passion for creating beautiful and intuitive user interfaces.", "$90,000 - $140,000", true);
+  private static final Offer firstTestOffer = new Offer(1L, "Vue Mid Sr. Developer",
+      "https://example.com/images/full-stack-developer.png",
+      "We're seeking a skilled full stack developer to join our team and help us build and maintain cutting-edge web applications. The ideal candidate will have experience with a range of technologies and be comfortable working in a fast-paced, agile environment.",
+      "$80,000 - $120,000", true);
+  private static final Offer secondTestOffer = new Offer(2L, "Software Engineer (React)",
+      "https://example.com/images/frontend-engineer.png",
+      "We're looking for a talented software engineer with a strong focus on frontend development to join our team. The ideal candidate will have experience building complex web applications with modern JavaScript frameworks, and a passion for creating beautiful and intuitive user interfaces.",
+      "$90,000 - $140,000", true);
   private static final List<Offer> offers = Arrays.asList(firstTestOffer, secondTestOffer);
 
   @BeforeEach
@@ -46,37 +52,44 @@ public class OffersIntegrationTest {
     repository.deleteAll();
     repository.flush();
   }
+
   @Test
   public void getAllOffersIntegrationTest() throws Exception {
     RequestBuilder request = get("/api/v1/offers").contentType(MediaType.APPLICATION_JSON);
 
     mvc.perform(request)
-      .andExpect(status().isOk())
-      .andExpect(jsonPath("$.content", hasSize(2)))
-      .andExpect(jsonPath("$.content[0].title", is(firstTestOffer.getTitle())))
-      .andExpect(jsonPath("$.content[1].title", is(secondTestOffer.getTitle())));
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.content", hasSize(2)))
+        .andExpect(jsonPath("$.content[0].title", is(firstTestOffer.getTitle())))
+        .andExpect(jsonPath("$.content[1].title", is(secondTestOffer.getTitle())));
   }
 
   @Test
   public void addOfferIntegrationTest() throws Exception {
-    OfferRequest dataScientistOffer = new OfferRequest("Data Scientist", "https://example.com/images/data-scientist.png", "We're seeking a skilled data scientist to help us analyze and interpret complex datasets and develop machine learning models to drive business insights. The ideal candidate will have a strong background in statistics and programming, and experience working with large datasets", "$100,000 - $150,000 per year", true);
+    OfferRequest dataScientistOffer = new OfferRequest("Data Scientist",
+        "https://example.com/images/data-scientist.png",
+        "We're seeking a skilled data scientist to help us analyze and interpret complex datasets and develop machine learning models to drive business insights. The ideal candidate will have a strong background in statistics and programming, and experience working with large datasets",
+        "$100,000 - $150,000 per year", true);
 
     RequestBuilder request = post("/api/v1/offers")
-      .contentType(MediaType.APPLICATION_JSON)
-      .content(JsonUtil.toJson(dataScientistOffer));
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(JsonUtil.toJson(dataScientistOffer));
 
     mvc.perform(request)
-      .andExpect(status().isOk())
-      .andExpect(jsonPath("$.title", is(dataScientistOffer.getTitle())));
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.title", is(dataScientistOffer.getTitle())));
   }
 
   @Test
   public void addOfferAlreadyExistingIntegrationTest() throws Exception {
-    OfferRequest offer2 = new OfferRequest("Vue Mid Sr. Developer", "https://example.com/images/full-stack-developer.png", "We're seeking a skilled full stack developer to join our team and help us build and maintain cutting-edge web applications. The ideal candidate will have experience with a range of technologies and be comfortable working in a fast-paced, agile environment.", "$100,000 - $150,000 per year", true );
+    OfferRequest offer2 = new OfferRequest("Vue Mid Sr. Developer",
+        "https://example.com/images/full-stack-developer.png",
+        "We're seeking a skilled full stack developer to join our team and help us build and maintain cutting-edge web applications. The ideal candidate will have experience with a range of technologies and be comfortable working in a fast-paced, agile environment.",
+        "$100,000 - $150,000 per year", true);
 
     RequestBuilder request = post("/api/v1/offers")
-      .contentType(MediaType.APPLICATION_JSON)
-      .content(JsonUtil.toJson(offer2));
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(JsonUtil.toJson(offer2));
 
     mvc.perform(request).andExpect(status().isBadRequest());
   }
--- api-jobs/src/test/java/ga/wawupc/api/jobs/service/OfferServiceImplTest.java
+++ api-jobs/src/test/java/ga/wawupc/api/jobs/service/OfferServiceImplTest.java
@@ -1,15 +1,13 @@
 package ga.wawupc.api.jobs.service;
 
-
 import ga.wawupc.api.jobs.domain.model.entity.Offer;
 import ga.wawupc.api.jobs.domain.persistence.OfferRepository;
-import ga.wawupc.api.jobs.service.OfferServiceImpl;
-
+import ga.wawupc.api.shared.exception.ResourceNotFoundException;
 import ga.wawupc.api.shared.exception.ResourceValidationException;
 
 import jakarta.validation.Validator;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,8 +15,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collections;
+import java.util.Optional;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class OfferServiceUnitTest {
@@ -28,15 +28,38 @@ class OfferServiceUnitTest {
   private Validator validator;
   @InjectMocks
   private OfferServiceImpl service;
+
+  Offer offer;
+
+  @BeforeEach
+  public void setUp() {
+    offer = new Offer(1L, "Full Stack Developer", "https://example.com/image.jpg",
+        "We're looking for a full stack developer with experience in Java and Angular.", "$125k-$127k", true);
+  }
+
   @Test
   public void createCompanyWithExistingNameShouldThrowException() {
-    Offer offer1 = new Offer(1L, "Full Stack Developer" , "https://example.com/image.jpg","We're looking for a full stack developer with experience in Java and Angular.", "$125k-$127k", true);
-    Offer offer2 = new Offer(2L, "Full Stack Developer" , "https://example.com/image.jpg","We're looking for a backend developer with experience in JavaScript and Vue.", "$200k-$250k", true);
+    Offer newOffer = new Offer(2L, "Full Stack Developer", "https://example.com/image.jpg",
+        "We're looking for a backend developer with experience in JavaScript and Vue.", "$200k-$250k", true);
 
-    when(repository.findByTitle(offer1.getTitle())).thenReturn(offer1);
+    when(repository.findByTitle(offer.getTitle())).thenReturn(offer);
 
-    when(validator.validate(offer2)).thenReturn(Collections.emptySet());
+    when(validator.validate(newOffer)).thenReturn(Collections.emptySet());
 
-    Assertions.assertThrows(ResourceValidationException.class, () -> service.create(offer2));
+    assertThrows(ResourceValidationException.class, () -> service.create(newOffer));
+  }
+
+  @Test
+  public void findByIdShouldReturnSingleOffer() {
+    when(repository.findById(1L)).thenReturn(Optional.of(offer));
+    
+    assertEquals(offer, service.getById(1L));
+  }
+
+  @Test
+  public void findByIdNonExistingOfferShouldThrowException(){
+    when(repository.findById(offer.getId())).thenReturn(Optional.empty());
+
+    assertThrows(ResourceNotFoundException.class, () -> service.getById(offer.getId()));
   }
 }
