387acbaaef293e1b38524a7e3b4a41c8793e6988
Diego Albitres <diego.albitres@gmail.com>
2023-06-20 22:57:33 -0500

    fix: added missing EnhancedModelMapper bean

--- api-jobs/src/main/java/ga/wawupc/api/jobs/mapping/MappingConfiguration.java
+++ api-jobs/src/main/java/ga/wawupc/api/jobs/mapping/MappingConfiguration.java
@@ -3,8 +3,15 @@ package ga.wawupc.api.jobs.mapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import ga.wawupc.api.shared.mapping.EnhancedModelMapper;
+
 @Configuration("offersMappingConfiguration")
 public class MappingConfiguration {
+  @Bean
+  public EnhancedModelMapper modelMapper() {
+    return new EnhancedModelMapper();
+  }
+
   @Bean
   public OfferMapper offerMapper() {
     return new OfferMapper();
